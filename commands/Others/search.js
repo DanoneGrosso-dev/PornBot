583403d7f862f17e37b4c3d68d77a2cd6fbe99d3
Harry <darkwolfinho@gmail.com>
2019-03-06 01:01:32 -0300

    alo

--- commands/Others/search.js
+++ commands/Others/search.js
@@ -8,7 +8,7 @@ module.exports = new (class cmd {
         this.aliases = ["procurar"]
     }
     run({ message, buildMessage, client, args }) {
-        if (!message.channel.nsfw) return message.channel.send(":underage: NSFW Command. Please switch to NSFW channel in order to use this command.")
+        //if (!message.channel.nsfw) return message.channel.send(":underage: NSFW Command. Please switch to NSFW channel in order to use this command.")
         let reason = args.join(" ");
 		if (reason.length < 1) return message.reply('**Please enter a text!**');
 
